e311b4a559bfe05ac9eca9983d201b761168f120
paran000ia <reyodzaki20100612@gmail.com>
2025-12-03 18:51:19 +0500

    new requests added

--- PTB.py
+++ PTB.py
@@ -1,7 +1,9 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
 import os
 import json
+info = []
+Ask_prob = 1
 
 API = os.getenv("API")
 admin = os.getenv("adminID")
@@ -18,7 +20,7 @@ class RepairRequest:
         pass
 
     def is_valid(self):
-        if self.device_type != "Телефон" or self.device_type != "Ноутбук" or self.device_type != "Планшет" or len(self.discription) < 10:
+        if self.device_type != "phone" or self.device_type != "laptop" or self.device_type != "tablet" or len(self.discription) < 10:
             normal = False
         else:
             normal = True
@@ -26,4 +28,26 @@ class RepairRequest:
     def to_dict(self):
         js = json.dumps(self)
 
+async def request(update, context):
+    await update.message.reply_text("Please, write your device type(laptop, phone or tablet)")
+    return Ask_prob
+
+async def requests_device(update, context):
+    await update.message.reply_text("Now, please write discription of your photo")
+    device = update.message.text
+    info.append(device)
+    return 2
+
+async def requests_prob(update, context):
+    await update.message.reply_text("OK. We will look at your problem and answer you later")
+    discr = update.message.text
+    info.append(discr)
+    return ConversationHandler.END
+
 app = ApplicationBuilder().token(API).build()
+app.add_handler(ConversationHandler(
+    entry_points=[CommandHandler("new_request", request)],
+    states={Ask_prob: [MessageHandler(filters.TEXT, requests_device)], 2: [MessageHandler(filters.TEXT, requests_prob)]},
+    fallbacks=["Error"]
+))
+app.run_polling()
